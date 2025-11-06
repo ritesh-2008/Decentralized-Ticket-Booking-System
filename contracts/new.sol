ef5e785c31d8676429fd1d2b268da4fa9bad2c41
ritesh-2008 <riteshvanivdekar@gmail.com>
2025-11-06 21:36:51 +0530

    intial commit

---
+++ contracts/new.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.7.0 <0.9.0;
+
+import "./ownable.sol";
+
+contract wallet is Ownable {
+ 
+ uint public eth = 0.001 ether;
+ uint public seats = 100;
+
+   
+
+    struct Ticket {
+       string name;
+       string email;
+       uint phoneNo;
+       uint  ticketNo;
+      }
+
+    Ticket[] public details;
+
+    function payforticket(string memory _name,
+       string memory _email,
+       uint _phoneNo) payable public {
+        require(msg.value == eth, "you have to pay exact amount of eth");
+        require(seats > 0);
+        seats -=1;
+        
+        details.push(Ticket({
+            name:_name,
+            email: _email,
+            phoneNo:_phoneNo,
+            ticketNo:100 - seats
+            }));
+
+    }
+
+    function withdraw() external onlyOwner {
+      address payable _owner = address(uint160(owner()));
+
+      _owner.transfer(address(this).balance);
+   }
+} 
