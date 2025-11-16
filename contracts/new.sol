3724734bd375c4e47dd2f9a52359d81268a01af5
ritesh-2008 <riteshvanivdekar@gmail.com>
2025-11-16 18:25:40 +0530

    additional feature

--- contracts/new.sol
+++ contracts/new.sol
@@ -4,45 +4,54 @@ pragma solidity >=0.7.0 <0.9.0;
 import "./ownable.sol";
 
 contract wallet is Ownable {
- 
- uint public eth = 0.001 ether;
- uint public ethforvip = 0.004 ether;
- uint public seats = 100;
- uint  public vipseats = 50;
+//  declare price,no of seats and event date
+ uint256 public eth = 0.001 ether;
+ uint256 public ethforvip = 0.004 ether;
+ uint256 public seats = 100;
+ uint256  public vipseats = 50;
  uint256 public  eventDate;
 
- 
+//  mapping 
  mapping (address => uint) public usertoTicket;
  mapping (address => uint) public usertoVipTicket;
 
+// event
   event PurchasedTicket(address indexed buyer,uint ticketno, uint amount);
   event Refund(address indexed user,uint ticketno,uint amount);
   event Withdraw(address indexed withdraweth , uint amount);
+  event ticketinfo(address indexed user,uint ticketno);
 
+// struct
     struct Ticket {
+      address buyer;
        string name;
        string email;
        uint phoneNo;
        uint  ticketNo;
+       bool refunded;
       }
 
+// store user data here
     Ticket[] public details;
     Ticket[] public vipdetails;
 
+// buy ticket here
     function payforticket(string memory _name,
        string memory _email,
-       uint _phoneNo) payable public {
+       uint _phoneNo) external  payable  {
         require(msg.value == eth, "you have to pay exact amount of eth");
         require(seats > 0,"seats are full");
-        require(usertoTicket[msg.sender] == 0,"you already own a ticket");
+        require(usertoTicket[msg.sender] == 0 && usertoVipTicket[msg.sender] == 0,"you already own a ticket");
         seats -=1;
-        uint TicketNO = 100 - seats;
+        uint TicketNO = details.length +1;
         
         details.push(Ticket({
+          buyer:msg.sender,
             name:_name,
             email: _email,
             phoneNo:_phoneNo,
-            ticketNo:TicketNO
+            ticketNo:TicketNO,
+            refunded:false
             }));
       usertoTicket[msg.sender] = TicketNO;
       emit PurchasedTicket(msg.sender, TicketNO, eth);
@@ -61,6 +70,12 @@ contract wallet is Ownable {
       usertoTicket[msg.sender] = 0;
       seats +=1;
 
+      uint256 idx = TicketNo - 1;
+      if (idx < details.length){
+        details[idx].refunded = true;
+      }
+      
+
       (bool sent,) =  payable(msg.sender).call{value: eth}("");
        require(sent,"error:issue with refund");
        
@@ -75,16 +90,18 @@ contract wallet is Ownable {
        uint _phoneNo)  payable public {
         require(msg.value == ethforvip, "you have to pay exact amount of eth");
         require( vipseats > 0,"seats are full");
-        require(usertoVipTicket[msg.sender] == 0,"you alredy own ticket");
+        require(usertoVipTicket[msg.sender] == 0 && usertoTicket[msg.sender] == 0,"you have to buy one ticket");
 
       vipseats -=1;
-        uint VipTicketNo = 50 - vipseats ;
+        uint VipTicketNo = vipdetails.length +1;
 
         vipdetails.push(Ticket({
+          buyer:msg.sender,
           name:_name,
           email:_email,
           phoneNo:_phoneNo,
-          ticketNo:VipTicketNo
+          ticketNo:VipTicketNo,
+          refunded:false
         }));
         usertoVipTicket[msg.sender] = VipTicketNo;
         emit PurchasedTicket(msg.sender,VipTicketNo , ethforvip);
@@ -98,17 +115,63 @@ contract wallet is Ownable {
       
       usertoVipTicket[msg.sender] = 0;
       vipseats+=1;
+
+        uint256 idx = ticketno - 1; // convert to 0-based
+        if (idx < vipdetails.length) {
+            vipdetails[idx].refunded = true;
+        }
   
      (bool sent,) = payable(msg.sender).call{value:ethforvip}("");
       require(sent,"error:issue with refund");
+
       emit Refund(msg.sender, ticketno, ethforvip);
    }
 
     //  withdraw the eth
       function withdraw() external onlyOwner {
+      require(block.timestamp > eventDate,"event is not over");
       uint amount = address(this).balance;
       (bool sent,) =  payable(owner()).call{value: amount}("");
       require(sent,"error:fail to withdraw ether");
+      
       emit Withdraw(owner(),amount);
+   }
+
+// check user information
+   function checkticket() external view  returns  (Ticket memory t) {
+    uint Ticketno=usertoTicket[msg.sender];
+    require(Ticketno > 0,"you dont own ticket");
+    uint256 idx =Ticketno-1;
+  require(idx < details.length, " invalid index");
+   return details[idx];
+   }
+
+// check vip user information
+   function checkticketvip() external view  returns  (Ticket memory t) {
+    uint Ticketno=usertoVipTicket[msg.sender];
+    require(Ticketno > 0,"you dont own ticket");
+    uint256 idx =Ticketno-1;
+    require(idx < vipdetails.length, " invalid index");
+   return vipdetails[idx];
+   }
+
+// check no of ticket sold
+   function soldticket() external view returns(uint256){
+    return  details.length;
+   }
+// check no of vip ticket sold
+   function soldvipticket() external view returns(uint256){
+    return vipdetails.length;
+   }
+
+// set ticket prize of regular and vip
+   function setticketprize(uint _ticket,uint _vipticket) external onlyOwner{
+    eth = _ticket;
+    ethforvip = _vipticket;
+   }
+
+// set event date 
+   function seteventdate(uint256 _eventdate) external onlyOwner{
+    eventDate = _eventdate;
    }
 } 
--- scripts/deploy_with_web3.ts
+++ scripts/deploy_with_web3.ts
@@ -6,7 +6,7 @@ import { deploy } from './web3-lib'
 
 (async () => {
   try {
-    const result = await deploy('Storage', [])
+    const result = await deploy('wallet', [])
     console.log(`address: ${result.address}`)
   } catch (e) {
     console.log(e.message)
